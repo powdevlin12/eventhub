a69f463bedfb93199fb8988234ebec3897f2586b
powdevlin12 <thudatdl123@gmail.com>
2025-05-02 17:08:25 +0700

    fix warning: Failed prop type: Invalid prop `color` supplied to `Text`: #3807A7A

--- src/common/constants/appColors.ts
+++ src/common/constants/appColors.ts
@@ -2,7 +2,7 @@ export const appColors = {
   primary: '#5669FF',
   white: '#fff',
   text: '#120D26',
-  gray: '#3807A7A',
+  gray: '#A1A1A1FF',
   grayStart: '#DADADA',
   errorStart: '#E94E4E',
   errorEnd: '#DF1616',
